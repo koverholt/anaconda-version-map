edfb2bd718d0cad4657e7b99ff8d6bf0d3c8a36d
Kristopher Overholt <koverholt@gmail.com>
2019-10-08 12:08:46 -0500

    Add script

---
+++ anaconda-versions.sh
@@ -0,0 +1,235 @@
+curl -O https://repo.anaconda.com/archive/Anaconda2-2019.07-Linux-x86_64.sh
+sudo bash Anaconda2-2019.07-Linux-x86_64.sh -bp /opt/python/Anaconda2-2019.07
+/opt/python/Anaconda2-2019.07/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
+sudo bash Anaconda3-2019.07-Linux-x86_64.sh -bp /opt/python/Anaconda3-2019.07
+/opt/python/Anaconda3-2019.07/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-2019.03-Linux-x86_64.sh
+sudo bash Anaconda2-2019.03-Linux-x86_64.sh -bp /opt/python/Anaconda2-2019.03
+/opt/python/Anaconda2-2019.03/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+sudo bash Anaconda3-2019.03-Linux-x86_64.sh -bp /opt/python/Anaconda3-2019.03
+/opt/python/Anaconda3-2019.03/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-2018.12-Linux-x86_64.sh
+sudo bash Anaconda2-2018.12-Linux-x86_64.sh -bp /opt/python/Anaconda2-2018.12
+/opt/python/Anaconda2-2018.12/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
+sudo bash Anaconda3-2018.12-Linux-x86_64.sh -bp /opt/python/Anaconda3-2018.12
+/opt/python/Anaconda3-2018.12/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-5.3.1-Linux-x86_64.sh
+sudo bash Anaconda2-5.3.1-Linux-x86_64.sh -bp /opt/python/Anaconda2-5.3.1
+/opt/python/Anaconda2-5.3.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.3.1-Linux-x86_64.sh
+sudo bash Anaconda3-5.3.1-Linux-x86_64.sh -bp /opt/python/Anaconda3-5.3.1
+/opt/python/Anaconda3-5.3.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-5.3.0-Linux-x86_64.sh
+sudo bash Anaconda2-5.3.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-5.3.0
+/opt/python/Anaconda2-5.3.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+sudo bash Anaconda3-5.3.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-5.3.0
+/opt/python/Anaconda3-5.3.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-5.2.0-Linux-x86_64.sh
+sudo bash Anaconda2-5.2.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-5.2.0
+/opt/python/Anaconda2-5.2.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+sudo bash Anaconda3-5.2.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-5.2.0
+/opt/python/Anaconda3-5.2.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-5.1.0-Linux-x86_64.sh
+sudo bash Anaconda2-5.1.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-5.1.0
+/opt/python/Anaconda2-5.1.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.1.0-Linux-x86_64.sh
+sudo bash Anaconda3-5.1.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-5.1.0
+/opt/python/Anaconda3-5.1.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-5.0.1-Linux-x86_64.sh
+sudo bash Anaconda2-5.0.1-Linux-x86_64.sh -bp /opt/python/Anaconda2-5.0.1
+/opt/python/Anaconda2-5.0.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+sudo bash Anaconda3-5.0.1-Linux-x86_64.sh -bp /opt/python/Anaconda3-5.0.1
+/opt/python/Anaconda3-5.0.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-5.0.0.1-Linux-x86_64.sh
+sudo bash Anaconda2-5.0.0.1-Linux-x86_64.sh -bp /opt/python/Anaconda2-5.0.0.1
+/opt/python/Anaconda2-5.0.0.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.0.0.1-Linux-x86_64.sh
+sudo bash Anaconda3-5.0.0.1-Linux-x86_64.sh -bp /opt/python/Anaconda3-5.0.0.1
+/opt/python/Anaconda3-5.0.0.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-5.0.0-Linux-x86_64.sh
+sudo bash Anaconda2-5.0.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-5.0.0
+/opt/python/Anaconda2-5.0.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-5.0.0-Linux-x86_64.sh
+sudo bash Anaconda3-5.0.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-5.0.0
+/opt/python/Anaconda3-5.0.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-4.4.0-Linux-x86_64.sh
+sudo bash Anaconda2-4.4.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-4.4.0
+/opt/python/Anaconda2-4.4.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-4.4.0-Linux-x86_64.sh
+sudo bash Anaconda3-4.4.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-4.4.0
+/opt/python/Anaconda3-4.4.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-4.3.1-Linux-x86_64.sh
+sudo bash Anaconda2-4.3.1-Linux-x86_64.sh -bp /opt/python/Anaconda2-4.3.1
+/opt/python/Anaconda2-4.3.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+sudo bash Anaconda3-4.3.1-Linux-x86_64.sh -bp /opt/python/Anaconda3-4.3.1
+/opt/python/Anaconda3-4.3.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-4.3.0-Linux-x86_64.sh
+sudo bash Anaconda2-4.3.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-4.3.0
+/opt/python/Anaconda2-4.3.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-4.3.0-Linux-x86_64.sh
+sudo bash Anaconda3-4.3.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-4.3.0
+/opt/python/Anaconda3-4.3.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-4.2.0-Linux-x86_64.sh
+sudo bash Anaconda2-4.2.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-4.2.0
+/opt/python/Anaconda2-4.2.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+sudo bash Anaconda3-4.2.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-4.2.0
+/opt/python/Anaconda3-4.2.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-4.1.1-Linux-x86_64.sh
+sudo bash Anaconda2-4.1.1-Linux-x86_64.sh -bp /opt/python/Anaconda2-4.1.1
+/opt/python/Anaconda2-4.1.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-4.1.1-Linux-x86_64.sh
+sudo bash Anaconda3-4.1.1-Linux-x86_64.sh -bp /opt/python/Anaconda3-4.1.1
+/opt/python/Anaconda3-4.1.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-4.1.0-Linux-x86_64.sh
+sudo bash Anaconda2-4.1.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-4.1.0
+/opt/python/Anaconda2-4.1.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-4.1.0-Linux-x86_64.sh
+sudo bash Anaconda3-4.1.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-4.1.0
+/opt/python/Anaconda3-4.1.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-4.0.0-Linux-x86_64.sh
+sudo bash Anaconda2-4.0.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-4.0.0
+/opt/python/Anaconda2-4.0.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-4.0.0-Linux-x86_64.sh
+sudo bash Anaconda3-4.0.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-4.0.0
+/opt/python/Anaconda3-4.0.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-2.5.0-Linux-x86_64.sh
+sudo bash Anaconda2-2.5.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-2.5.0
+/opt/python/Anaconda2-2.5.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2.5.0-Linux-x86_64.sh
+sudo bash Anaconda3-2.5.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-2.5.0
+/opt/python/Anaconda3-2.5.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-2.4.1-Linux-x86_64.sh
+sudo bash Anaconda2-2.4.1-Linux-x86_64.sh -bp /opt/python/Anaconda2-2.4.1
+/opt/python/Anaconda2-2.4.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2.4.1-Linux-x86_64.sh
+sudo bash Anaconda3-2.4.1-Linux-x86_64.sh -bp /opt/python/Anaconda3-2.4.1
+/opt/python/Anaconda3-2.4.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda2-2.4.0-Linux-x86_64.sh
+sudo bash Anaconda2-2.4.0-Linux-x86_64.sh -bp /opt/python/Anaconda2-2.4.0
+/opt/python/Anaconda2-2.4.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2.4.0-Linux-x86_64.sh
+sudo bash Anaconda3-2.4.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-2.4.0
+/opt/python/Anaconda3-2.4.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-2.3.0-Linux-x86_64.sh
+sudo bash Anaconda-2.3.0-Linux-x86_64.sh -bp /opt/python/Anaconda-2.3.0
+/opt/python/Anaconda-2.3.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2.3.0-Linux-x86_64.sh
+sudo bash Anaconda3-2.3.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-2.3.0
+/opt/python/Anaconda3-2.3.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-2.2.0-Linux-x86_64.sh
+sudo bash Anaconda-2.2.0-Linux-x86_64.sh -bp /opt/python/Anaconda-2.2.0
+/opt/python/Anaconda-2.2.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2.2.0-Linux-x86_64.sh
+sudo bash Anaconda3-2.2.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-2.2.0
+/opt/python/Anaconda3-2.2.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-2.1.0-Linux-x86_64.sh
+sudo bash Anaconda-2.1.0-Linux-x86_64.sh -bp /opt/python/Anaconda-2.1.0
+/opt/python/Anaconda-2.1.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2.1.0-Linux-x86_64.sh
+sudo bash Anaconda3-2.1.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-2.1.0
+/opt/python/Anaconda3-2.1.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-2.0.1-Linux-x86_64.sh
+sudo bash Anaconda-2.0.1-Linux-x86_64.sh -bp /opt/python/Anaconda-2.0.1
+/opt/python/Anaconda-2.0.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2.0.1-Linux-x86_64.sh
+sudo bash Anaconda3-2.0.1-Linux-x86_64.sh -bp /opt/python/Anaconda3-2.0.1
+/opt/python/Anaconda3-2.0.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-2.0.0-Linux-x86_64.sh
+sudo bash Anaconda-2.0.0-Linux-x86_64.sh -bp /opt/python/Anaconda-2.0.0
+/opt/python/Anaconda-2.0.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2.0.0-Linux-x86_64.sh
+sudo bash Anaconda3-2.0.0-Linux-x86_64.sh -bp /opt/python/Anaconda3-2.0.0
+/opt/python/Anaconda3-2.0.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-1.9.2-Linux-x86_64.sh
+sudo bash Anaconda-1.9.2-Linux-x86_64.sh -bp /opt/python/Anaconda-1.9.2
+/opt/python/Anaconda-1.9.2/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-1.9.1-Linux-x86_64.sh
+sudo bash Anaconda-1.9.1-Linux-x86_64.sh -bp /opt/python/Anaconda-1.9.1
+/opt/python/Anaconda-1.9.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-1.9.0-Linux-x86_64.sh
+sudo bash Anaconda-1.9.0-Linux-x86_64.sh -bp /opt/python/Anaconda-1.9.0
+/opt/python/Anaconda-1.9.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-1.8.0-Linux-x86_64.sh
+sudo bash Anaconda-1.8.0-Linux-x86_64.sh -bp /opt/python/Anaconda-1.8.0
+/opt/python/Anaconda-1.8.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-1.7.0-Linux-x86_64.sh
+sudo bash Anaconda-1.7.0-Linux-x86_64.sh -bp /opt/python/Anaconda-1.7.0
+/opt/python/Anaconda-1.7.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-1.6.1-Linux-x86_64.sh
+sudo bash Anaconda-1.6.1-Linux-x86_64.sh -bp /opt/python/Anaconda-1.6.1
+/opt/python/Anaconda-1.6.1/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-1.6.0-Linux-x86_64.sh
+sudo bash Anaconda-1.6.0-Linux-x86_64.sh -bp /opt/python/Anaconda-1.6.0
+/opt/python/Anaconda-1.6.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-1.5.0-Linux-x86_64.sh
+sudo bash Anaconda-1.5.0-Linux-x86_64.sh -bp /opt/python/Anaconda-1.5.0
+/opt/python/Anaconda-1.5.0/bin/python -V
+
+curl -O https://repo.anaconda.com/archive/Anaconda-1.4.0-Linux-x86_64.sh
+sudo bash Anaconda-1.4.0-Linux-x86_64.sh -bp /opt/python/Anaconda-1.4.0
+/opt/python/Anaconda-1.4.0/bin/python -V
